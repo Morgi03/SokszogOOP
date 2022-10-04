b4bf8809c554bb5c9c6a7247c271a7c7b5189d9d
Morgován Nimród <morgovannimrod22@gmail.com>
2022-10-04 15:41:26 +0200

    Haromszog osztaly kiegészítése Sokszog -> Absztrakt osztály toString()-ek megírásának befejezése

--- src/hu/petrik/sokszogoop/Teglalap.java
+++ src/hu/petrik/sokszogoop/Teglalap.java
@@ -27,6 +27,6 @@ public class Teglalap extends Sokszog {
 
     @Override
     public String toString() {
-        return String.format("Téglalap: a = %f - b = %f - K = %f - T = %f", this.getA(), this.b, this.getKerulet(), this.getTerulet());
+        return String.format("Téglalap: a = %-10.3f  b = %-10.3f  %s", this.getA(), this.b, super.toString());
     }
 }
