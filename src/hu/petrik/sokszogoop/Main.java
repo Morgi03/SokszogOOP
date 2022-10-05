7ba8628fe5cfad147d5ae288631f33e5a5bbeeab
Morgován Nimród <morgovannimrod22@gmail.com>
2022-10-05 18:22:49 +0200

    Paralelogramma osztály elkészítése Osztály tesztelése

--- src/hu/petrik/sokszogoop/Main.java
+++ src/hu/petrik/sokszogoop/Main.java
@@ -2,14 +2,20 @@ package hu.petrik.sokszogoop;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Háromszög teszt ---------------------------------------\n");
         for (int i = 0; i < 100; i++) {
             Haromszog h = new Haromszog();
             System.out.println(h);
         }
+        System.out.println("\nTéglalap teszt ---------------------------------------\n");
         for (int i = 0; i < 5; i++) {
             double a = Math.random() * 10 + 5;
             double b = Math.random() * 10 + 5;
             System.out.println(new Teglalap(a, b));
+        }
+        System.out.println("\nParalelogramma teszt ---------------------------------------\n");
+        for (int i = 0; i < 16; i++) {
+            System.out.println(new Paralelogramma());
         }
     }
 }
--- src/hu/petrik/sokszogoop/Paralelogramma.java
+++ src/hu/petrik/sokszogoop/Paralelogramma.java
@@ -1,15 +1,26 @@
 package hu.petrik.sokszogoop;
 
-public class Paralelogramma extends Sokszog{
+public class Paralelogramma extends Sokszog {
     private double b;
+    private double alfa;
 
     public Paralelogramma(double a, double b) {
         super(a);
         this.b = b;
     }
 
-    public Paralelogramma(){
+    public Paralelogramma() {
+        super(veletlenOldal());
+        this.b = veletlenOldal();
+        this.alfa = veletlenSzog();
+    }
 
+    private static double veletlenOldal() {
+        return Math.random() * 10 + 5;
+    }
+
+    private static double veletlenSzog() {
+        return Math.random() * 89;
     }
 
     public double getB() {
@@ -20,16 +31,24 @@ public class Paralelogramma extends Sokszog{
         this.b = b;
     }
 
-    public double getKerulet(){
-    return 2*(this.getA() + this.b);
+    public double getAlfa() {
+        return alfa;
     }
 
-    public double getTerulet(){
+    public void setAlfa(double alfa) {
+        this.alfa = alfa;
+    }
 
+    public double getKerulet() {
+        return 2 * (this.getA() + this.b);
+    }
+
+    public double getTerulet() {
+        return this.getA() * this.b *Math.sin(Math.toRadians(this.alfa));
     }
 
     @Override
     public String toString() {
-        return "Paralelogramma{}";
+        return String.format("Paralelogramma: a = %-10.3f  b = %-10.3f  a és b által közbezárt szög = %-10.3f  %s", this.getA(), this.getB(), this.getAlfa(),super.toString());
     }
 }
